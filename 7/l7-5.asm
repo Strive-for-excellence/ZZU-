44fa5eb5fa58f7a4bac127c4ee9dcd11cc5ab29a
Strive-for-excellence <1505656319@qq.com>
2019-01-01 17:24:19 +0800

    Create l7-5.asm

---
+++ 7/l7-5.asm
@@ -0,0 +1,87 @@
+		include io16.inc
+		
+		.data
+		
+		intmsg byte ' A 8259 A Interrupt!',0dh,0ah,0
+		
+		counter byte 0
+		.code
+start:
+		mov ax,@data
+		mov ds,ax
+		mov ax,3508h
+		int 21h
+		push es
+		push bx
+		cli
+		push ds
+		mov ax,seg new08h
+		mov ds,ax
+		mov dx,offset new08h
+		mov ax,2508h
+		int 21h
+		pop ds
+		in al,21h
+		push ax
+		and al,0feh
+		out 21h,al
+		mov counter,0
+		sti
+start1:	
+		cmp counter,10
+		jb start1
+		cli
+		pop ax
+		out 21h,al
+		pop dx
+		pop ds
+		mov ax,2508h
+		int 21h
+		sti
+		exit 0
+new08h	proc
+		sti
+		push ax
+		push si
+		push ds
+		mov ax,@data
+		mov ds,ax
+		
+		inc counter 
+		test counter,1
+		jnz nxt
+		push eax
+		mov al,counter
+		call dispuib
+		pop eax
+		
+		mov si,offset intmsg
+		call dpstri
+nxt:		
+		mov al,20h
+		out 20h,al
+		pop ds
+		pop si
+		pop ax
+		iret
+		
+new08h	endp
+
+dpstri	proc
+		push ax
+		push bx
+dps1:	mov al,[si]
+		cmp al,0
+		jz dps2
+		mov bx,0
+		mov ah,0eh
+		int 10h
+		inc si
+		jmp dps1
+dps2:	pop bx
+		pop ax
+		ret 
+dpstri  endp
+		
+		
+		end start
